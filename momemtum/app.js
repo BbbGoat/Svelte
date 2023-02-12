7116062c5cbd3f77e09329711bac18040a3b8aea
BbbGoat <oomi9421@gmail.com>
2023-02-12 21:33:28 +0900

    ss

--- momemtum/app.js
+++ momemtum/app.js
@@ -1,9 +1,17 @@
 const loginFrom = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
     event.preventDefault();
     console.log(loginInput.value);
 }
 
+function handleLinkClick(event) {
+    console.log(event);
+    alert("clicked!");
+}
+
 loginFrom.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
