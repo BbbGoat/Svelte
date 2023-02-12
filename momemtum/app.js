b86a0c8aebff0acfa7b6fc783f8dd20841524f6c
BbbGoat <oomi9421@gmail.com>
2023-02-12 21:23:37 +0900

    ss

--- momemtum/app.js
+++ momemtum/app.js
@@ -1,16 +1,9 @@
-const age = parseInt (prompt("몇살이야?"));
+const loginFrom = document.querySelector("#login-form")
+const loginInput = document.querySelector("#login-form input");
 
-if (isNaN(age)) {
-    console.log("숫자를 적어주세요");
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log(loginInput.value);
 }
-else if (age < 18 && age > 0) {
-    console.log("너무어립니다!");
-}
-else if (age <= 0) {
-    console.log("시간여행을 하시는군요!");
-}
-else {
-    console.log("음주가능!");
-}
-console.log(age);
-// console.log(typeof age, parseInt(age));
+
+loginFrom.addEventListener("submit", onLoginSubmit);
