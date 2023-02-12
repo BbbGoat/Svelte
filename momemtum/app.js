09bef74d720bfa008f5a461bbe04d90f5cde78bc
BbbGoat <oomi9421@gmail.com>
2023-02-12 22:34:55 +0900

    ss

--- momemtum/app.js
+++ momemtum/app.js
@@ -1,17 +1,35 @@
-const loginFrom = document.querySelector("#login-form")
+const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username"
+// 일반적으로 string만 포함된 변수는 대문자로 표기하고,
+// string을 저장하고 싶을 때 사용하는 관습이 있다!
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(loginInput.value);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    
+    paintGreetings(username);
 }
 
-function handleLinkClick(event) {
-    console.log(event);
-    alert("clicked!");
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginFrom.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    // show the greetings
+    paintGreetings(savedUsername);
+}
