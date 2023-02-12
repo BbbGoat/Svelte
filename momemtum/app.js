b8c1119ee8d4121c656ae2658bd0568ba24f1cb4
BbbGoat <oomi9421@gmail.com>
2023-02-12 18:01:17 +0900

    ss

--- momemtum/app.js
+++ momemtum/app.js
@@ -1,3 +1,16 @@
-const amIFat = null;
-let something;
-console.log(something);
+const age = parseInt (prompt("몇살이야?"));
+
+if (isNaN(age)) {
+    console.log("숫자를 적어주세요");
+}
+else if (age < 18 && age > 0) {
+    console.log("너무어립니다!");
+}
+else if (age <= 0) {
+    console.log("시간여행을 하시는군요!");
+}
+else {
+    console.log("음주가능!");
+}
+console.log(age);
+// console.log(typeof age, parseInt(age));
