2fe6f3b4f74dbe1a7e2f592fb5460a6f0a02cf39
BbbGoat <oomi9421@gmail.com>
2023-02-18 20:04:17 +0900

    Update todo.js

--- momemtum/js/todo.js
+++ momemtum/js/todo.js
@@ -3,17 +3,37 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+// 로컬저장소에 넣을 배열값 저장해두는 변수
+const toDos = [];
+
+// 4. 로컬저장소에 todo list 저장하는 함수
+function saveToDos() {
+    // 로컬저장소에 해당 key, 값을 저장하는 의미인데
+    // 순서상 이미 들어가있는 toDos 안의 array를 불러와 로컬저장소에 찍어주는것
+    localStorage.setItem("todos", toDos);
+}
+
+// 3. 버튼 클릭시 해당 버튼 지우기 함수
+function deleteToDo(event) {
+    // 이벤트가 발생한 타겟의 부모요소를 선택한다 -> 타겟 버튼의 부모요소인 li 선택
+    const li = event.target.parentElement;
+    li.remove();
+}
 
 // 2. 전달받은 value를 화면에 출력시키기
 function paintToDo(newTodo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
-
-    // li의 뒤에 자식요소로 span태그를 추가한다
-    li.appendChild(span);
-
-    // 추가한 span태그 안에 최종적으로 아래에서 받은 전달변수를 출력시킨다!
+    const button = document.createElement("button");
+    
+    // 추가한 span태그 안에 아래에서 받은 전달변수를 출력시킨다!
     span.innerText = newTodo;
+    button.innerText = "👻";
+    button.addEventListener("click", deleteToDo);
+
+    // li의 뒤에 자식요소로 span, button 태그를 추가한다
+    li.appendChild(span);
+    li.appendChild(button);
     
     // 출력확인하기
     // console.log(li);
@@ -28,8 +48,16 @@ function handleToDoSubmit(event) {
     const newTodo = toDoInput.value;
     toDoInput.value = "";
 
-    // 다른 함수를 호출하고 거기에 인수를 전달한다
+    // 입력받은 텍스트를 로컬저장 배열변수에 추가하기
+    toDos.push(newTodo);
+    
+    // 함수 호출하기
+    // 1. 다른 함수를 호출하고 거기에 인수를 전달한다
     paintToDo(newTodo);
+    // 2. 로컬저장소 함수 호출
+    // 호출되는 시점에는 이미 newTodo는 toDos의 array에 들어있다
+    saveToDos();
+
 }
     
 
